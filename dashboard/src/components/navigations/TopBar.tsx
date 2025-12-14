67a6705438449fcf4c67a0e2bcf33d3206a2ac20
team-btg <bryan_gomez032000@yahoo.com>
2025-12-14 15:37:17 +0800

    feat: Remove main content section from TopBarLayout component

--- dashboard/src/components/navigations/TopBar.tsx
+++ dashboard/src/components/navigations/TopBar.tsx
@@ -70,11 +70,7 @@ const TopBarLayout: React.FC<TopBarLayoutProps> = ({
           <button onClick={onSettings} title="Settings" className="bg-transparent p-1 rounded-lg text-gray-400 hover:text-blue-400 focus:outline-none"><Settings /></button>
           <button onClick={onLogout} title="Logout" className="bg-transparent p-1 rounded-lg text-gray-400 hover:text-red-400 focus:outline-none"><LogOut /></button>
         </div>
-      </div>
-      {/* Main Content */}
-      <div className="p-4">
-        {selectedServerId ? children : <div className="text-center mt-20 text-gray-400">Select a server to view dashboard.</div>}
-      </div>
+      </div> 
     </div>
   );
 };
